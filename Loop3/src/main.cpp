0ee5c0bd1532202c14b959fc2f62c6da2a56aafc
maxsos <kolievmax@gmail.com>
2020-11-04 19:36:02 +0300

    task 3 done

--- Loop3/src/main.cpp
+++ Loop3/src/main.cpp
@@ -7,7 +7,7 @@
 
 void calc(double* arr, uint32_t ySize, uint32_t xSize, int rank, int size)
 {
-  if (rank == 0 && size > 0)
+  if (size == 1)
   {
     for (uint32_t y = 4; y < ySize; y++)
     {
@@ -16,6 +16,81 @@ void calc(double* arr, uint32_t ySize, uint32_t xSize, int rank, int size)
         arr[y*xSize + x] = sin(arr[(y - 4)*xSize + x]);
       }
     }
+    return;
+  }
+
+  MPI_Bcast(&ySize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&xSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+
+  if (rank == 0) {
+    size_t begin_int[size + 1];
+    for (size_t i = 0; i < size + 1; i++)
+    {
+      begin_int[i] = xSize / size * i + ((xSize % size > i) ? i : xSize % size);
+    }
+    
+    for (size_t i = 1; i < size; ++i) {
+      for (size_t j = begin_int[i]; j < begin_int[i+1]; ++j)
+      {
+        double array[] = {arr[j],
+                    arr[xSize + j],
+                    arr[2*xSize + j],
+                    arr[3*xSize + j]};
+        MPI_Send(&array, 4, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+      }
+    }
+    
+    for (size_t i = begin_int[0]; i < begin_int[1]; ++i)
+      for (size_t j = 4; j < ySize; ++j) 
+      {
+        arr[j*xSize + i] = sin(arr[(j - 4)*xSize + i]);
+      }
+    double buf[ySize];
+
+    for (size_t i = 1; i < size; ++i) {
+      for (size_t j = begin_int[i]; j < begin_int[i+1]; ++j) 
+      {
+        MPI_Recv(&buf, ySize, MPI_DOUBLE, i, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        for(size_t t = 0; t < ySize; ++t)
+          arr[t*xSize + j] = buf[t];
+      }
+    }
+
+  } else {
+    size_t begin_int = xSize / size * rank + ((xSize % size > rank) ? rank : (xSize % size));     
+    size_t end_int = xSize / size * (rank + 1) + ((xSize % size > rank) ? rank + 1 : xSize % size);
+
+    size_t len = end_int - begin_int;
+
+    double* data = (double *)malloc(ySize * len * sizeof(double));
+
+    for(size_t i = 0; i < len; ++i) 
+    {
+      int staus;
+      double buff[] = {0, 0, 0, 0};
+      MPI_Recv(&buff, 4, MPI_DOUBLE, 
+               0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      data[i * ySize] = buff[0];
+      data[i * ySize + 1] = buff[1];
+      data[i * ySize + 2] = buff[2];
+      data[i * ySize + 3] = buff[3];
+    }
+  
+    for(size_t x = 0; x < len; ++x)
+      for (size_t y = 4; y < ySize; ++y)
+      {
+        data[x*ySize + y] = sin(data[x*ySize + y - 4]);
+      }
+  
+    for(size_t i = 0; i < len; ++i) 
+    {
+      MPI_Send((data + i*ySize), ySize, MPI_DOUBLE, 
+               0, 0, MPI_COMM_WORLD);          
+    }
+
+    free(data);
   }
 }
 
