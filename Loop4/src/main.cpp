656947f3a544b3979e7f0da44747f296ae28d803
maxsos <kolievmax@gmail.com>
2020-11-27 00:35:01 +0300

    finally add 4

--- Loop4/src/main.cpp
+++ Loop4/src/main.cpp
@@ -4,10 +4,11 @@
 #include <mpi.h>
 #include <unistd.h>
 #include <cmath>
+#include <vector>
 
 void calc(double* arr, uint32_t zSize, uint32_t ySize, uint32_t xSize, int rank, int size)
 {
-  if (size >= 1) {
+  if (size == 1) {
     for (uint32_t z = 1; z < zSize; z++) {
       for (uint32_t y = 0; y < ySize - 1; y++) {
         for (uint32_t x = 0; x < xSize - 1; x++) {
@@ -22,88 +23,92 @@ void calc(double* arr, uint32_t zSize, uint32_t ySize, uint32_t xSize, int rank,
   MPI_Bcast(&ySize, 1, MPI_INT, 0, MPI_COMM_WORLD);
   MPI_Bcast(&xSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-  if (rank == 0) {   
+
+  if (rank == 0) {
     int *sendcounts = (int *)malloc(size * sizeof(int)),
         *rcounts = (int *)malloc(size * sizeof(int)),
         *displs = (int *)malloc(size * sizeof(int));
     double* recvbuf;
     size_t end_int, begin_int;
     
-    
     for (size_t i = 0; i < size; i++)
     {
-      begin_int = zSize / size * i + ((zSize % size > i) ? i : zSize % size);
-      end_int = zSize / size * (i + 1) + ((zSize % size > i) ? i + 1 : zSize % size);
-      displs[i] = begin_int *  xSize * ySize;
-      sendcounts[i] = (end_int - begin_int + ((i+1 < size) ? 1 : 0)) * xSize * ySize;
-      rcounts[i] = (end_int - begin_int)* xSize * ySize;
+      begin_int = ySize / size * i + ((ySize % size > i) ? i : ySize % size);
+      end_int = ySize / size * (i + 1) + ((ySize % size > i) ? i + 1 : ySize % size);
+      displs[i] = begin_int * xSize;
+      sendcounts[i] = (end_int - begin_int + ((i+1 < size) ? 1 : 0)) * xSize;
+      rcounts[i] = (end_int - begin_int)* xSize;
     } 
-    begin_int = zSize / size * 1 + ((zSize % size > 1) ? 1 : zSize % size);
+    if (rcounts[size - 1] == xSize)
+      rcounts[size-1] = 0;
+    begin_int = ySize / size * 1 + ((ySize % size > 1) ? 1 : ySize % size);
 
     recvbuf = (double *)malloc(sendcounts[0] * sizeof(double));
 
-    MPI_Scatterv(arr, sendcounts, displs, MPI_DOUBLE, 
+    double *buf = (double *)malloc(ySize * sizeof(double));
+    double *ptr = arr;
+
+    for (uint32_t z = 0; z < zSize - 1; z++)
+    {
+          
+      MPI_Scatterv(ptr, sendcounts, displs, MPI_DOUBLE,
                  recvbuf, sendcounts[0], MPI_DOUBLE,
                  0, MPI_COMM_WORLD);
 
-    
-    // for (size_t y = 0; y < begin_int; ++y)
-    //   for (size_t x = 3; x < xSize; ++x) 
-    //   {
-    for (uint32_t z = 1; z < begin_int; z++) {
-      for (uint32_t y = 0; y < ySize - 1; y++) {
-        for (uint32_t x = 0; x < xSize - 1; x++) {
-         recvbuf[z*ySize*xSize + y*xSize + x] = 
-                sin(
-                  recvbuf[(z - 1)*ySize*xSize + (y + 1)*xSize + x + 1]
-                );
+      ptr += xSize*ySize;
+      for (size_t y = 0; y < ySize; ++y)
+        buf[y] = arr[(z+1)*xSize*ySize + y*xSize + xSize -1];
+
+      for (size_t y = 0; y < begin_int; ++y)
+        for (size_t x = 0; x < xSize - 1; ++x) 
+        {
+          recvbuf[y*xSize + x] = sin(recvbuf[(y + 1)*xSize + x + 1]);
         }
-      }
+      
+
+      MPI_Gatherv(recvbuf, rcounts[0], MPI_DOUBLE,
+                  ptr, rcounts, displs, MPI_DOUBLE, 
+                  0, MPI_COMM_WORLD);
+
+      for (size_t y = 0; y < ySize; ++y)
+        arr[(z+1)*xSize*ySize + y*xSize + xSize -1] = buf[y];
     }
 
-    MPI_Gatherv(recvbuf, rcounts[0], MPI_DOUBLE,
-                arr, rcounts, displs, MPI_DOUBLE, 
-                0, MPI_COMM_WORLD);
-
-
+    free(buf);
     free(recvbuf);
     free(sendcounts);
     free(displs);    
     free(rcounts);
-
   } else {
-    size_t begin_int = zSize / size * rank + ((zSize % size > rank) ? rank : (zSize % size));     
-    size_t end_int = zSize / size * (rank + 1) + ((zSize % size > rank) ? rank + 1 : zSize % size);
-
+    size_t begin_int = ySize / size * rank + ((ySize % size > rank) ? rank : (ySize % size));     
+    size_t end_int = ySize / size * (rank + 1) + ((ySize % size > rank) ? rank + 1 : ySize % size);
     size_t len = end_int - begin_int + ((rank + 1 < size) ? 1 : 0);
-
-    double* recvbuf = (double *)malloc(ySize * xSize * len * sizeof(double));
-
-    MPI_Scatterv(nullptr, nullptr, nullptr, MPI_DOUBLE, 
-                 recvbuf, len * xSize * ySize, MPI_DOUBLE,
+  
+    double* recvbuf = (double *)malloc(xSize * len * sizeof(double));
+    for (uint32_t z = 1; z < zSize; z++)
+    {
+      MPI_Scatterv(nullptr, nullptr, nullptr, MPI_DOUBLE, 
+                 recvbuf, len * xSize, MPI_DOUBLE,
                  0, MPI_COMM_WORLD);
 
-    for (uint32_t z = 1; z < len; z++) {
-      for (uint32_t y = 0; y < ySize - 1; y++) {
-        for (uint32_t x = 0; x < xSize - 1; x++) {
-         recvbuf[z*ySize*xSize + y*xSize + x] = 
-                sin(
-                  recvbuf[(z - 1)*ySize*xSize + (y + 1)*xSize + x + 1]
-                );
+      for (size_t y = 0; y < len - 1; ++y) 
+        for (size_t x = 0; x < xSize - 1; ++x) 
+        {
+          recvbuf[y*xSize + x] = sin(recvbuf[(y + 1)*xSize + x + 1]);
+          
         }
-      }
+      if (len == 1 && rank + 1 < size)  
+        MPI_Gatherv(recvbuf, xSize, MPI_DOUBLE,
+                  nullptr, nullptr, nullptr, MPI_DOUBLE, 
+                  0, MPI_COMM_WORLD);  
+      else
+        MPI_Gatherv(recvbuf, (len - 1) * xSize, MPI_DOUBLE,
+                  nullptr, nullptr, nullptr, MPI_DOUBLE, 
+                  0, MPI_COMM_WORLD);  
+
     }
-
-    if (rank + 1 < size)
-      --len;
-
-    MPI_Gatherv(recvbuf, len * xSize * ySize, MPI_DOUBLE,
-                nullptr, nullptr, nullptr, MPI_DOUBLE, 
-                0, MPI_COMM_WORLD);          
-
     free(recvbuf);
   }
-
 
 }
 
